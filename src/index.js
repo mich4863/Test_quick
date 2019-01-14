5669e585b0401e6de07b6915656e3c07cb236ed5
Michael Overgaard Nielsen <mich4863@edu.campusvejle.dk>
2019-01-14 12:56:08 +0100

    bootstrap installed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import './scss/main.scss'
 // import babel-polyfill for async function declearation and await operator
 import 'babel-polyfill'
+
+// import stylesheet
+import './scss/main.scss'
+
 // Import JS files
+import 'bootstrap'
